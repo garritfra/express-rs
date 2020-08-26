3bc10d367d243ec7c129fd368d1f406d13d1be0d
garritfra <garritfranke@gmail.com>
2020-08-26 22:14:35 +0200

    feat: request header parsing

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,5 @@
+use crate::Method::GET;
 use std::io::Read;
-use std::io::Write;
 use std::net::TcpListener;
 
 pub struct Express {/* TODO */}
@@ -23,12 +23,56 @@ impl Express {
         for stream in listener.incoming() {
             let mut buffer = [0; 1024];
             stream.unwrap().read(&mut buffer).unwrap();
-            println!("Request: {}", String::from_utf8_lossy(&buffer[..]));
+            let request = Request::from_string(String::from_utf8_lossy(&buffer[..]).to_string());
+            println!("Request: {:?}", request);
         }
     }
 }
 
-pub struct Request {/* TODO */}
+#[derive(Debug)]
+enum Method {
+    GET,
+    UNKNOWN,
+    // ...
+}
+
+#[derive(Debug)]
+pub struct Request {
+    method: Method,
+    path: String,
+    version: String,
+}
+
+/*
+Request: GET / HTTP/1.1
+Host: localhost:8080
+Connection: keep-alive
+Cache-Control: max-age=0
+Upgrade-Insecure-Requests: 1
+User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.135 Safari/537.36
+Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng
+Sec-Fetch-Site: cross-site
+Sec-Fetch-Mode: navigate
+Sec-Fetch-Dest: document
+Accept-Encoding: gzip, deflate, br
+Accept-Language: en-CA,en;q=0.9,de-DE;q=0.8,de;q=0.7,en-GB;q=0.6,en-US;q=0.5,la;q=0.4
+Cookie: _ga=GA1.1.783631210.1557768372
+*/
+
+impl Request {
+    pub fn from_string(b: String) -> Self {
+        let fields: Vec<&str> = b.split_whitespace().collect();
+        Request {
+            method: match fields.get(0).unwrap() {
+                &"GET" => Method::GET,
+                _ => Method::UNKNOWN,
+            },
+            path: fields.get(1).unwrap().to_string(),
+            version: fields.get(2).unwrap().to_string(),
+        }
+    }
+}
+
 pub struct Response {/* TODO */}
 
 impl Response {
