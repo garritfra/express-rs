08032fe08617f7635d838f44fd93aa7c03d72d19
garritfra <garritfranke@gmail.com>
2020-08-27 11:07:44 +0200

    test: listen

--- src/lib.rs
+++ src/lib.rs
@@ -121,8 +121,31 @@ impl Response {
 
 #[cfg(test)]
 mod tests {
+    use super::*;
+    use std::sync::mpsc::channel;
+
     #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
+    #[should_panic]
+    fn port_out_of_range_min() {
+        let app = Express::new();
+        app.listen(0)
+    }
+
+    #[test]
+    fn process_running() {
+        let (tx, rx) = channel();
+
+        std::thread::spawn(move || {
+            let app = Express::new();
+            app.listen(65535);
+
+            tx.send("Thread exited").unwrap()
+        });
+
+        // If a value is received in less than 1 second, we can assume that the process has finished running.
+        let did_exit = rx.recv_timeout(std::time::Duration::from_secs(1));
+
+        // Assert that the process did not exit
+        assert_eq!(did_exit.is_ok(), false);
     }
 }
