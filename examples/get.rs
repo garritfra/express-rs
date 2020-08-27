396da3435d61c1ba690fb133fa723b1f9a098eeb
Garrit Franke <garritfranke@gmail.com>
2020-08-27 15:23:05 +0200

    feat: expand example

--- examples/get.rs
+++ examples/get.rs
@@ -3,6 +3,9 @@ use express_rs::Express;
 fn main() {
     let mut app = Express::new();
     app.get("/", |_, mut res| res.send("Hello World!".to_string()));
+    app.get("/hello", |_, mut res| {
+        res.send("Hi from /hello!".to_string())
+    });
 
     app.listen(8080);
 }
