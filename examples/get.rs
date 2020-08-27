eaa9f8e686a936f71b3f0dca7e0fca42c9a1141f
Garrit Franke <garritfranke@gmail.com>
2020-08-27 15:52:44 +0200

    fix: warning

--- examples/get.rs
+++ examples/get.rs
@@ -2,8 +2,8 @@ use express_rs::Express;
 
 fn main() {
     let mut app = Express::new();
-    app.get("/", |_, mut res| res.send("Hello World!".to_string()));
-    app.get("/hello", |_, mut res| {
+    app.get("/", |_, res| res.send("Hello World!".to_string()));
+    app.get("/hello", |_, res| {
         res.send("<h1>Hi from /hello!</h1>".to_string())
     });
 
