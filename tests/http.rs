8dde0a480e681d2e6a9e0e86a7704b4a4b3d0588
Garrit Franke <garritfranke@gmail.com>
2020-08-28 23:39:37 +0200

    fix(http): body parsing

--- tests/http.rs
+++ tests/http.rs
@@ -11,5 +11,18 @@ mod tests {
         assert_eq!(request.method, http::Method::GET);
         assert_eq!(request.path, "/");
         assert_eq!(request.version, "HTTP/1.1".to_string());
+        assert_eq!(request.body, None);
+    }
+
+    #[test]
+    fn request_from_string_valid_with_body() {
+        let string = "GET / HTTP/1.1\n\r\n\rthis is the body".to_string();
+
+        let request = http::Request::from_string(string).unwrap();
+
+        assert_eq!(request.method, http::Method::GET);
+        assert_eq!(request.path, "/");
+        assert_eq!(request.version, "HTTP/1.1".to_string());
+        assert_eq!(request.body, Some("this is the body".to_string()));
     }
 }
