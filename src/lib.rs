1125dcfb3278212bf90b36a108ece856992f5a07
garritfra <garritfranke@gmail.com>
2020-08-28 18:45:32 +0200

    chore: extract http functions

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,6 @@
+mod http;
+
+use http::{Method, Request, Response};
 use std::fmt::Debug;
 use std::io::Read;
 use std::io::Write;
@@ -101,67 +104,5 @@ impl Debug for Mount {
             .field("method", &self.method)
             .field("path", &self.path)
             .finish()
-    }
-}
-
-/// Common HTTP Methods
-///
-/// If a method is needed, which is not specified here,
-/// `Method::UNKNOWN(String)` can be used
-#[derive(Debug, PartialEq)]
-pub enum Method {
-    GET,
-    POST,
-    PUT,
-    PATCH,
-    DELETE,
-    UNKNOWN(String),
-}
-
-/// Represents a HTTP request
-#[derive(Debug, PartialEq)]
-pub struct Request {
-    method: Method,
-    path: String,
-    version: String,
-}
-
-impl Request {
-    /// takes an request as string and parses all relevant fields
-    pub fn from_string(b: String) -> Self {
-        let fields: Vec<&str> = b.split_whitespace().collect();
-        Request {
-            method: match fields.get(0).unwrap() {
-                &"GET" => Method::GET,
-                &"POST" => Method::POST,
-                &"PUT" => Method::PUT,
-                &"PATCH" => Method::PATCH,
-                &"DELETE" => Method::DELETE,
-                method => Method::UNKNOWN(method.to_string()),
-            },
-            path: fields.get(1).unwrap().to_string(),
-            version: fields.get(2).unwrap().to_string(),
-        }
-    }
-}
-
-/// Represents a HTTP response
-#[derive(Debug, PartialEq)]
-pub struct Response {
-    stream: String,
-    headers: Vec<String>,
-}
-
-impl Response {
-    pub fn new() -> Self {
-        Self {
-            stream: String::new(),
-            headers: Vec::new(),
-        }
-    }
-
-    /// Writes plain text to the response buffer
-    pub fn send(&mut self, s: String) {
-        self.stream.push_str(&s);
     }
 }
